5debf5d6af4531a0969649be73706c30e3fbc6a6
Cho_SungMin <58628847+ahsjdkflgh3041@users.noreply.github.com>
2023-08-29 21:57:07 +0900

    [Feature] 시간 구현
    -오브젝트 매니저
    -시간 매니저
    -시간 영향을 받는 오브젝트가 상속할 스크립트

---
+++ Assets/SM/Scripts/TimeInfluenced.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TimeInfluenced : MonoBehaviour
+{
+	#region PublicVariables
+	#endregion
+	protected int m_objectTime
+	{
+		get
+		{
+			return m_time;
+		}
+		set
+		{
+			m_time = value;
+			UpdateTimeState();
+		}
+	}
+	public abstract void UpdateTimeState();
+
+	public void PlusTime()
+	{
+		++m_objectTime;
+	}
+	public void MinusTime()
+	{
+		--m_objectTime;
+	}
+	#region PrivateVariables
+
+	private int m_time;
+	#endregion
+	#region PublicMethod
+	#endregion
+	#region PrivateMethod
+	#endregion
+}
---
+++ Assets/SM/Scripts/TimeManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    #region PublicVariables
+    public static TimeManager s_Instance
+    {
+        get 
+        {
+            if (s_instance == null)
+            {
+                return null;
+            }
+            return s_instance;
+        }
+    }
+    #endregion
+    #region PrivateVariables
+    [SerializeField] private int m_timeCount;
+
+    private static TimeManager s_instance;
+
+    private List<TimeInfluenced> m_levelObjects;
+    #endregion
+    #region PublicMethod
+
+    public void PlusTime()
+    {
+        foreach(TimeInfluenced obj in m_levelObjects)
+        {
+            obj.PlusTime();
+            ++m_timeCount;
+        }
+    }
+    public void MinusTIme()
+    {
+        foreach (TimeInfluenced obj in m_levelObjects)
+        {
+            obj.MinusTime();
+            --m_timeCount;
+        }
+    }
+
+    public void GetObjects(List<TimeInfluenced> _objects)
+    {
+        m_levelObjects = _objects;
+    }
+    #endregion
+    #region PrivateMethod
+    private void Awake()
+    {
+        if (s_instance == null)
+        {
+            s_instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    #endregion
+}
