776302dd80ec6e9ffd852fad3d7d46686fc428f7
Minkyung <kyoongeeee@gmail.com>
2023-08-30 17:51:36 +0900

    Dotween 패키지 임포트, 문 에셋

--- Assets/Scripts/MK/Door.cs
+++ Assets/Scripts/MK/Door.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Door : MonoBehaviour, ISwitchConnectedObjects
 {
@@ -8,6 +9,8 @@ public class Door : MonoBehaviour, ISwitchConnectedObjects
     #endregion
     #region PrivateVariables
     [SerializeField] private bool m_isSwitchConnected= false;
+    [SerializeField] private Transform m_door;
+    [SerializeField] private Collider m_doorPortal;
     #endregion
     #region PublicMethod
     public void InteractStart()
@@ -15,10 +18,28 @@ public class Door : MonoBehaviour, ISwitchConnectedObjects
         if (!m_isSwitchConnected)
         {
             m_isSwitchConnected = true;
+
+            if (m_door != null)
+            {
+                RotateDoor();
+            }
+            else
+            {
+                Debug.Log("No child object 'door' found.");
+            }
         }
     }
     #endregion
     #region PrivateMethod
+    private void Start()
+    {
+        m_door = transform.Find("door");
+    }
+
+    private void RotateDoor()
+    {
+        m_door.DORotate(new Vector3(0f, 100f, 0f), 2.0f);
+    }
     #endregion
 
 
