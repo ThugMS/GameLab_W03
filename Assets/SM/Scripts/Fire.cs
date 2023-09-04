f237213a8fd32a5f07a64b2fb6ac566a95c956e5
MinseokLee <leems041625@gmail.com>
2023-09-04 15:48:45 +0900

    [Fix] : 노인 불에 죽는모션 수정

--- Assets/SM/Scripts/Fire.cs
+++ Assets/SM/Scripts/Fire.cs
@@ -14,18 +14,37 @@ public class Fire : MonoBehaviour
     #region PrivateMethod
     private void OnCollisionEnter(Collision m_collision)
     {
-        IBurn burnCheck;
-        m_collision.gameObject.TryGetComponent(out burnCheck);
-        if (burnCheck != null)
-        {
-            burnCheck.Burn();
-        }
+        //IBurn burnCheck;
+        //m_collision.gameObject.TryGetComponent(out burnCheck);
+        //if (burnCheck != null)
+        //{
+        //    burnCheck.Burn();
+        //}
+
         /*
         else if(collision.gameObject.layer == 3)
         { 
             this.gameObject.SetActive(false);
         }
         */
+
+        Youth youth;
+
+        m_collision.gameObject.TryGetComponent(out youth);
+
+        if (youth != null)
+        {
+            youth.Death();
+        }
+
+        Senior player;
+
+        m_collision.gameObject.TryGetComponent(out player);
+
+        if(player != null)
+        {
+            player.Death();
+        }
     }
     #endregion
 }
